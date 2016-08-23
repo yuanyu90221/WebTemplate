b78e5e61826d9e6403f103f986092d5c2f4c4adf
TommyYehCool <tommy.yeh1112@gmail.com>
2016-08-23 14:35:12 +0800

    將 css 套入登入頁面

--- src/main/java/com/exfantasy/template/config/WebSecurityConfig.java
+++ src/main/java/com/exfantasy/template/config/WebSecurityConfig.java
@@ -35,7 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
         	.authorizeRequests()
-            	.antMatchers("/", "/home", "/user/register").permitAll()
+            	.antMatchers("/", "/home", "/css", "/user/register").permitAll()
             	.anyRequest().authenticated()
             	.and()
             .formLogin()
